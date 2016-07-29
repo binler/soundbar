a49a9db804e8276e2df52169ef9df046ff7c9686
binler <letatbinh130794@gmail.com>
2016-07-29 16:17:27 +0700

    first commit

---
+++ myfunc.js
@@ -0,0 +1,10 @@
+var sound = new Audio('keysd.mp3');
+sound.volume = 1;
+
+// $('body').keydown(function(e) {
+//     if(e.keyCode >= 65 && e.keyCode <= 90) {
+//       e.preventDefault();
+//       if (sound.currentTime) sound.currentTime = 0;
+//       sound.play();
+//     }
+// });
