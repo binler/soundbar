12aa32f54ce138be5b5d6efe25eb4e8ea0ecfdab
binler <letatbinh130794@gmail.com>
2016-08-05 17:29:07 +0700

    fix animnate

--- myfunc.js
+++ myfunc.js
@@ -1,10 +1,26 @@
 var sound = new Audio('keysd.mp3');
 sound.volume = 1;
+var txt = '';
+$('body').keydown(function(e) {
 
-// $('body').keydown(function(e) {
-//     if(e.keyCode >= 65 && e.keyCode <= 90) {
-//       e.preventDefault();
-//       if (sound.currentTime) sound.currentTime = 0;
-//       sound.play();
-//     }
-// });
+    if(e.keyCode >= 65 && e.keyCode <= 90) {
+      e.preventDefault();
+      if (sound.currentTime) sound.currentTime = 0;
+      sound.play();
+      txt = txt + e.key;
+      $('.container').html(txt);
+      showImage(e.key,txt,'a',0);
+    }
+});
+
+function showImage(key, text, char, index) {
+  var firstChar = text.charAt(index);
+  if(key == char) {
+    if(firstChar == char){
+      console.log(txt + txt.length);
+      $('.fam').addClass('active');
+    }
+  } else {
+    $('.fam').removeClass('active');
+  }
+}
